360b1a10c745c20e7c3c00da6c3d5a324467661b
gfreycode <gfreycode@gmail.com>
2021-09-10 06:45:08 -0500

    Fallo

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -14,7 +14,6 @@ module.exports = {
         assetModuleFilename: 'assets/images/[hash][ext][query]',
     },
     mode: 'development',
-    watch: true,
     resolve: {
         extensions: ['.js', '.ts'],
         alias: {
@@ -80,4 +79,10 @@ module.exports = {
         }),
         new Dotenv(),
     ],
+    devServer: {
+        static: path.join(__dirname, 'dist'),
+        compress: true,
+        historyApiFallback: true,
+        port: 3010,
+    },
 }
